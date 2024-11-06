0b3957d97b3fc215b26ab81a4f98be2a95c62b7c
unknown <mahshidkhodsiani@yahoo.com>
2024-11-06 15:49:17 +0330

    001-amooz

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     <h3 class="text-white text-center mb-4">Admin Panel</h3>
-    <a href="index.php" data-page="index" class="sidebar-link"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-    <a href="user.php" data-page="user" class="sidebar-link"><i class="fas fa-users"></i> Users</a>
-    <a href="settings.php" data-page="settings" class="sidebar-link"><i class="fas fa-cog"></i> Settings</a>
-    <a href="reports.php" data-page="reports" class="sidebar-link"><i class="fas fa-chart-line"></i> Reports</a>
+    <a href="index.php" data-page="index" class="sidebar-link"><i class="fas fa-book"></i> آموزش ها</a>
+    <a href="user.php" data-page="user" class="sidebar-link"><i class="fas fa-user"></i> پروفایل</a>
+    <a href="reports.php" data-page="reports" class="sidebar-link"><i class="fas fa-chart-line"></i> پورسانت ها</a>
+    <a href="settings.php" data-page="settings" class="sidebar-link"><i class="fas fa-cog"></i> تنظیمات</a>
 </div>
